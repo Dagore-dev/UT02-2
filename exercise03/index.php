96af27b5b0f931d0eb32c4b31682a065fa7f6d1c
Dagore-dev <david.gomez.redondo.dev@gmail.com>
2022-10-18 08:43:43 +0200

    exercise03

--- exercise03/index.php
+++ exercise03/index.php
@@ -1,3 +1,10 @@
 <?php require_once('../templates/header.php'); print_header('Exercise 03') ?>
 
+<form action="./process.php" method="post">
+  <label for="week-day">Enter a number between 1-7</label>
+  <input type="number" name="week-day" id="week-day" min="1" max="7">
+
+  <button>Submit</button>
+</form>
+
 <?php require_once('../templates/footer.html') ?>
--- exercise03/process.php
+++ exercise03/process.php
@@ -1,3 +1,14 @@
 <?php require_once('../templates/header.php'); print_header('Exercise 03') ?>
 
+<?php
+  $week_day = $_POST['week-day'];
+  $week_day_array = ['1' => 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+  if (isset($week_day))
+  {
+    echo "<p>$week_day_array[$week_day]</p>";
+  }
+
+?>
+
 <?php require_once('../templates/footer.html') ?>
