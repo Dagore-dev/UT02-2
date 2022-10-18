29b83cd3b3d49d3acee128318016e0d06315c1c2
Dagore-dev <david.gomez.redondo.dev@gmail.com>
2022-10-18 08:35:06 +0200

    exercise02

--- exercise02/index.php
+++ exercise02/index.php
@@ -1,3 +1,10 @@
 <?php require_once('../templates/header.php'); print_header('Exercise 02') ?>
 
+<form action="./process.php" method="post">
+  <label for="hours">Enter the hour and I salute you</label>
+  <input type="number" name="hours" id="hours" min="0" max="23">
+
+  <button>Submit</button>
+</form>
+
 <?php require_once('../templates/footer.html') ?>
--- exercise02/process.php
+++ exercise02/process.php
@@ -1,3 +1,19 @@
 <?php require_once('../templates/header.php'); print_header('Exercise 02') ?>
 
+<?php
+  $hours = $_POST['hours'];
+
+  if (isset($hours))
+  {
+    $greet =  match ($hours) {
+      '6', '7', '8', '9', '10', '11', '12' => 'Good morning',
+      '13', '14', '15', '16', '17', '18', '19', '20' => 'Good afternoon',
+      '21', '22', '23', '0', '1', '2', '3', '4', '5' => 'Good night',
+      default => 'An integer between 0 and 23 (both included) is expected'
+    };
+
+    echo "<p>$greet.</p>";
+  }
+?>
+
 <?php require_once('../templates/footer.html') ?>
