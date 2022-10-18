165ebe31d2102b39875bd910022b45a08c314d15
Dagore-dev <david.gomez.redondo.dev@gmail.com>
2022-10-18 09:01:15 +0200

    exercise04

--- exercise04/index.php
+++ exercise04/index.php
@@ -1,3 +1,10 @@
 <?php require_once('../templates/header.php'); print_header('Exercise 04') ?>
 
+<form action="./process.php" method="post">
+  <label for="hours">Introduce your worked hours this week.</label>
+  <input type="number" name="hours" id="hours">
+
+  <button>Submit</button>
+</form>
+
 <?php require_once('../templates/footer.html') ?>
--- exercise04/process.php
+++ exercise04/process.php
@@ -1,3 +1,25 @@
 <?php require_once('../templates/header.php'); print_header('Exercise 04') ?>
 
+<?php
+  $hours = $_POST['hours'];
+  $regular_hour_payment = 12;
+  $overtime_hour_payment = 16;
+
+  if(isset($hours))
+  {
+    $regular_payment = $hours * $regular_hour_payment;
+    $overtime_hours = $hours - 40;
+    $overtime_payment = 0;
+
+    if ($overtime_hours > 0)
+    {
+      $overtime_payment = $overtime_hours * ($overtime_hour_payment - $regular_hour_payment);
+    }
+
+    $total_payment = $regular_payment + $overtime_payment;
+
+    echo "<p>Your weekly salary goes up to $total_payment euros</p>";
+  }
+?>
+
 <?php require_once('../templates/footer.html') ?>
