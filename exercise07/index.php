b0a8906ec50980e2aa58d12da52d05a8f534c1c6
Dagore-dev <david.gomez.redondo.dev@gmail.com>
2022-10-18 09:49:12 +0200

    exercise07

--- exercise07/index.php
+++ exercise07/index.php
@@ -1,3 +1,16 @@
 <?php require_once('../templates/header.php'); print_header('Exercise 07') ?>
 
+<form action="./process.php" method="post">
+  <label for="grade1">Grade 1</label>
+  <input type="number" name="grade1" id="grade1" step="any">
+
+  <label for="grade2">Grade 2</label>
+  <input type="number" name="grade2" id="grade2" step="any">
+
+  <label for="grade3">Grade 3</label>
+  <input type="number" name="grade3" id="grade3" step="any">
+
+  <button>Submit</button>
+</form>
+
 <?php require_once('../templates/footer.html') ?>
--- exercise07/process.php
+++ exercise07/process.php
@@ -1,3 +1,17 @@
 <?php require_once('../templates/header.php'); print_header('Exercise 07') ?>
 
+<?php
+  $grade1 = $_POST['grade1'];
+  $grade2 = $_POST['grade2'];
+  $grade3 = $_POST['grade3'];
+
+  if (isset($grade1, $grade2, $grade3))
+  {
+    $sum = $grade1 + $grade2 + $grade3;
+    $result = number_format($sum / 3, 2);
+
+    echo "<p>The mean of $grade1, $grade2, $grade3 is equal to $result</p>";
+  }
+?>
+
 <?php require_once('../templates/footer.html') ?>
