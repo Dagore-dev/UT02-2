1d47e4bfccbe4dcaecdffc728af4db67ea2dbfc1
Dagore-dev <david.gomez.redondo.dev@gmail.com>
2022-10-18 08:18:42 +0200

    exercise01

--- exercise01/index.php
+++ exercise01/index.php
@@ -1,3 +1,16 @@
 <?php require_once('../templates/header.php'); print_header('Exercise 01') ?>
 
+<form action="./process.php" method="post">
+  <label for="week-day">Week day</label>
+  <select name="week-day" id="week-day">
+    <option value="0">Monday</option>
+    <option value="1">Tuesday</option>
+    <option value="2">Wednesday</option>
+    <option value="3">Thursday</option>
+    <option value="4">Friday</option>
+  </select>
+
+  <button>Submit</button>
+</form>
+
 <?php require_once('../templates/footer.html') ?>
--- exercise01/process.php
+++ exercise01/process.php
@@ -1,3 +1,14 @@
 <?php require_once('../templates/header.php'); print_header('Exercise 01') ?>
 
+<?php
+  $week_day = $_POST['week-day'];
+  $week_day_array = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+  $week_day_subjects = ['Entrepreneurship', 'Backend', 'Backend', 'English', 'English'];
+
+  if (isset($week_day)) 
+  {
+    echo "<p>$week_day_array[$week_day] we study $week_day_subjects[$week_day] in the first hour.</p>";
+  }
+?>
+
 <?php require_once('../templates/footer.html') ?>
