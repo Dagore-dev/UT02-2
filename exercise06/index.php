63699a707150fc1dcdaa4ff7a58f59306133209f
Dagore-dev <david.gomez.redondo.dev@gmail.com>
2022-10-18 09:31:35 +0200

    exercise06

--- exercise06/index.php
+++ exercise06/index.php
@@ -1,3 +1,10 @@
 <?php require_once('../templates/header.php'); print_header('Exercise 06') ?>
 
+<form action="./process.php" method="post">
+  <label for="height">Introduce the height</label>
+  <input type="number" name="height" id="height" step="any" min="0">
+
+  <button>Submit</button>
+</form>
+
 <?php require_once('../templates/footer.html') ?>
--- exercise06/process.php
+++ exercise06/process.php
@@ -1,3 +1,15 @@
 <?php require_once('../templates/header.php'); print_header('Exercise 06') ?>
 
+<?php
+$height = $_POST['height'];
+$g = 9.81;
+
+if (isset($height))
+{
+  $time = number_format(sqrt((2 * $height) / $g), 2);
+  echo "<p>It takes $time seconds to fall down.</p>";
+}
+
+?>
+
 <?php require_once('../templates/footer.html') ?>
