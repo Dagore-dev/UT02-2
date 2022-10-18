5ffce5a8e6d2d80f55e28a64c92ad29c81156a09
Dagore-dev <david.gomez.redondo.dev@gmail.com>
2022-10-18 09:19:42 +0200

    exercise05

--- exercise05/index.php
+++ exercise05/index.php
@@ -1,3 +1,17 @@
 <?php require_once('../templates/header.php'); print_header('Exercise 05') ?>
 
+<form action="./process.php" method="post">
+  <p><code>
+    ax + b = 0
+  </code></p>
+
+  <label for="a">a</label>
+  <input type="number" name="a" id="a">
+
+  <label for="a">b</label>
+  <input type="number" name="b" id="b">
+
+  <button>Submit</button>
+</form>
+
 <?php require_once('../templates/footer.html') ?>
--- exercise05/process.php
+++ exercise05/process.php
@@ -1,3 +1,25 @@
 <?php require_once('../templates/header.php'); print_header('Exercise 05') ?>
 
+<?php
+  $a = $_POST['a'];
+  $b = $_POST['b'];
+
+  if(isset($a, $b))
+  {
+    $result = '';
+
+    if ($a != 0)
+    {
+      $x = number_format((-1 * $b) / $a, 2);
+      $result = "{$a}x + $b = 0 -> x = $x";
+    }
+    else
+    {
+      $result = "There is no solution, just b = $b";
+    }
+
+    echo "<p>$result.</p>";
+  }
+?>
+
 <?php require_once('../templates/footer.html') ?>
