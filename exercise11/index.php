523669c7ead884593822db477dcbb9803f0991bf
Dagore <david.gomez.redondo.dev@gmail.com>
2022-10-20 22:20:03 +0200

    exercise11

--- exercise11/index.php
+++ exercise11/index.php
@@ -1,3 +1,13 @@
 <?php require_once('../templates/header.php'); print_header('Exercise 11') ?>
 
+<form action="./process.php" method="post">
+  <label for="hour">Hour</label>
+  <input type="number" name="hour" id="hour" min="0" max="23">
+
+  <label for="minutes">Minutes</label>
+  <input type="number" name="minutes" id="minutes" min="0" max="59">
+
+  <button>Submit</button>
+</form>
+
 <?php require_once('../templates/footer.html') ?>
--- exercise11/process.php
+++ exercise11/process.php
@@ -1,3 +1,19 @@
 <?php require_once('../templates/header.php'); print_header('Exercise 11') ?>
 
+<?php
+
+$hour = $_POST['hour'];
+$minutes = $_POST['minutes'];
+
+if (isset($hour, $minutes))
+{
+  $remaining_hours = 23 - $hour;
+  $remaining_minutes = 60 - $minutes;
+  $remaining_seconds_until_midnight = ($remaining_hours * 3600) + ($remaining_minutes * 60);
+
+  printf('<p>From %s:%s to midnight there are %s seconds.</p>', str_pad($hour, 2, '0'), str_pad($minutes, 2, '0'), $remaining_seconds_until_midnight);
+}
+
+?>
+
 <?php require_once('../templates/footer.html') ?>
