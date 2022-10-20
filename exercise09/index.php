2fb231cd1322cbc1830e72be5733081a2b64d0cf
Dagore <david.gomez.redondo.dev@gmail.com>
2022-10-20 15:48:55 +0200

    exercise09

--- exercise09/index.php
+++ exercise09/index.php
@@ -1,3 +1,20 @@
 <?php require_once('../templates/header.php'); print_header('Exercise 09') ?>
 
+<form action="./process.php" method="POST">
+  <p>
+    ax2 + bx +c = 0
+  </p>
+
+  <label for="a">a</label>
+  <input type="number" name="a" id="a">
+
+  <label for="b">b</label>
+  <input type="number" name="b" id="b">
+
+  <label for="c">c</label>
+  <input type="number" name="c" id="c">
+
+  <button>Submit</button>
+</form>
+
 <?php require_once('../templates/footer.html') ?>
--- exercise09/process.php
+++ exercise09/process.php
@@ -1,3 +1,36 @@
 <?php require_once('../templates/header.php'); print_header('Exercise 09') ?>
 
+<?php
+  $a = $_POST['a'];
+  $b = $_POST['b'];
+  $c = $_POST['c'];
+
+  if (isset($a, $b, $c))
+  {
+    $inner_sqrt = (($b * $b) - (4 * $a * $c));
+    
+    if ($inner_sqrt >= 0)
+    {
+        $sqrt = sqrt($inner_sqrt);
+        $positive_result = number_format((-$b + $sqrt) / (2 * $a), 2);
+
+        if ($inner_sqrt == 0)
+        {
+          echo "<p>The given equation has only one solution: $positive_result</p>";
+        }
+        else
+        {
+          $negative_result = number_format((-$b - $sqrt) / (2 * $a), 2);
+
+          echo "The given equation has two solutions: $positive_result and $negative_result";
+        }
+
+    }
+    else
+    {
+      echo '<p>The given equation doesn\'t have real solution.</p>';
+    }
+  }
+?>
+
 <?php require_once('../templates/footer.html') ?>
