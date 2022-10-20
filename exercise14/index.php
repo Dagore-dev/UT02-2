8bf5f4f744e3345ec181e32d44af70512dbea542
Dagore <david.gomez.redondo.dev@gmail.com>
2022-10-20 22:50:17 +0200

    exercise14

--- exercise14/index.php
+++ exercise14/index.php
@@ -1,3 +1,10 @@
 <?php require_once('../templates/header.php'); print_header('Exercise 14') ?>
 
+<form action="./process.php" method="post">
+  <label for="number">Number</label>
+  <input type="number" name="number" id="number">
+
+  <button>Submit</button>
+</form>
+
 <?php require_once('../templates/footer.html') ?>
--- exercise14/process.php
+++ exercise14/process.php
@@ -1,3 +1,21 @@
 <?php require_once('../templates/header.php'); print_header('Exercise 14') ?>
 
+<?php
+
+$number = $_POST['number'];
+
+if (isset($number))
+{
+  $output = match(true) {
+    ($number % 2 === 0 && $number % 5 === 0) => "$number is even and divisible by 5",
+    $number % 2 === 0 => "$number is even",
+    $number % 5 === 0 => "$number is divisible by 5",
+    default => "$number is not a integer"
+  };
+
+  echo "<p>$output.</p>";
+}
+
+?>
+
 <?php require_once('../templates/footer.html') ?>
