db865e7d26d37c488e35364c538b23617069b5d6
Dagore <david.gomez.redondo.dev@gmail.com>
2022-10-20 22:35:40 +0200

    exercise13

--- exercise13/index.php
+++ exercise13/index.php
@@ -1,3 +1,16 @@
 <?php require_once('../templates/header.php'); print_header('Exercise 13') ?>
 
+<form action="./process.php" method="post">
+  <label for="num1">Integer 1</label>
+  <input type="number" name="num1" id="num1">
+
+  <label for="num2">Integer 2</label>
+  <input type="number" name="num2" id="num2">
+
+  <label for="num3">Integer 3</label>
+  <input type="number" name="num3" id="num3">
+
+  <button>Submit</button>
+</form>
+
 <?php require_once('../templates/footer.html') ?>
--- exercise13/process.php
+++ exercise13/process.php
@@ -1,3 +1,25 @@
 <?php require_once('../templates/header.php'); print_header('Exercise 13') ?>
 
+<?php
+
+  $num1 = $_POST['num1'];
+  $num2 = $_POST['num2'];
+  $num3 = $_POST['num3'];
+
+  if (isset($num1, $num2, $num3))
+  {
+    $array = [$num1, $num2, $num3];
+    sort($array);
+    $output = '';
+
+    foreach ($array as $value)
+    {
+      $output .= $value . ' ';
+    }
+
+    echo "<p>Sorted in ascending order: $output</p>";
+  }
+
+?>
+
 <?php require_once('../templates/footer.html') ?>
