6ac1dfb33f9bcbb5fc94ee9aa87f35d7030418cb
Dagore <david.gomez.redondo.dev@gmail.com>
2022-10-21 14:52:28 +0200

    exercise18

--- exercise18/process.php
+++ exercise18/process.php
@@ -1,3 +1,28 @@
 <?php require_once('../templates/header.php'); print_header('Exercise 18') ?>
 
+<?php
+  $int = $_POST['int'];
+
+  if (isset($int))
+  {
+    $n = abs((int) $int);
+    $digits = 0;
+
+    if ($n === 0)
+    {
+      $digits = 1;
+    }
+    else
+    {
+      while ($n > 0)
+      {
+        $n = intdiv($n, 10);
+        $digits++;
+      }
+    }
+
+    echo "<p>$int has $digits digits.</p>";
+  }
+?>
+
 <?php require_once('../templates/footer.html') ?>
