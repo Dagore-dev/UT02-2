b7ea29471d0d1a8effb106d6741c825d6ddea3e6
Dagore <david.gomez.redondo.dev@gmail.com>
2022-10-21 14:38:57 +0200

    exercise16

--- exercise16/index.php
+++ exercise16/index.php
@@ -1,3 +1,10 @@
 <?php require_once('../templates/header.php'); print_header('Exercise 16') ?>
 
+<form action="./process.php" method="post">
+  <label for="int">Integer</label>
+  <input type="number" name="int" id="int">
+
+  <button>Submit</button>
+</form>
+
 <?php require_once('../templates/footer.html') ?>
--- exercise16/process.php
+++ exercise16/process.php
@@ -1,3 +1,13 @@
 <?php require_once('../templates/header.php'); print_header('Exercise 16') ?>
 
+<?php
+  $int = $_POST['int'];
+
+  if (isset($int))
+  {
+    $last_digit = $int[strlen($int) - 1];
+    echo "<p>The last digit of $int is $last_digit.</p>";
+  }
+?>
+
 <?php require_once('../templates/footer.html') ?>
