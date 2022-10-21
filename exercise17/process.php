85bd258601d5d09d15e47ec55a434d25d77ba605
Dagore <david.gomez.redondo.dev@gmail.com>
2022-10-21 14:41:55 +0200

    exercise17

--- exercise17/process.php
+++ exercise17/process.php
@@ -1,3 +1,13 @@
 <?php require_once('../templates/header.php'); print_header('Exercise 17') ?>
 
+<?php
+  $int = $_POST['int'];
+
+  if (isset($int))
+  {
+    $last_digit = $int[0];
+    echo "<p>The first digit of $int is $last_digit.</p>";
+  }
+?>
+
 <?php require_once('../templates/footer.html') ?>
